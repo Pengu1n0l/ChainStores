7e94df5055a68f7eab1ab6d71c6465d7beccbc0e
Pengu1n0l <moral21@mail.ru>
2021-05-15 16:52:20 +0800

    Добавьте файлы проекта.

---
+++ ChainStores/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ChainStores.Core
+{
+    class ObservableObject : INotifyPropertyChanged //Механизм привязки изменения элемента формы
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));// вызвать делегат который принадлежит основному дескриптору
+        }
+    }
+}
